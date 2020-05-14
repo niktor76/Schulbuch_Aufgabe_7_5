aad21e0804b8a061bf7af15493434a70274c4dc9
viktor-nbubar <viktor.steck@gmail.com>
2020-05-14 20:26:38 +0200

    And so, it begins.

---
+++ cmake-build-debug/CMakeFiles/Schulbuch_Aufgabe_7_5.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Schulbuch_Aufgabe_7_5.dir/main.cpp.o"
+  "Schulbuch_Aufgabe_7_5"
+  "Schulbuch_Aufgabe_7_5.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Schulbuch_Aufgabe_7_5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
